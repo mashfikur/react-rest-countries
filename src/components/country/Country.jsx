71e7ce8cf9a183ee6bdb9c45529b541a92142398
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-09-10 14:23:01 +0600

    added toggle button

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react"
 import "./Country.css"
 
 const Country = ({country}) => {
+  const [visited,setVisited]=useState(false)
+
+  const handleVisit=()=>{
+    setVisited(!visited)
+  }
+
   return (
     <div className="country" >
       <h4> Name : {country?.name?.common}  </h4>
       <img src={country?.flags?.png} alt={country?.flags?.alt} />
       <p>Capital : {country?.capital} </p>
       <p>Code : {country?.cca2} </p>
+      <button onClick={handleVisit} >{visited?" Visited":"Going"}</button>
+      <h3> {visited?"I have visited":"I am going to visit"} </h3>
     </div>
   )
 }
